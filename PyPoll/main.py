e1d9f67005e395b8161a8abf6a689a067c89a684
spmims <stephenpmims@gmail.com>
2023-03-01 14:47:18 -0600

    Progress on the process

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,13 +6,35 @@ import os
 csvpath = os.path.join("python-challenge", "PyPoll", "Resources", "election_data.csv")
 
 # Assigning variable, list, and dictionaries
-candidate_names = []
+candidate_list = []
 votes = {}
 percent = {}
 total_votes = 0
 
+# Read in CSV
 with open(csvpath) as voting_file:
     csvreader = csv.reader(voting_file, delimiter = ",")
+    
+    print(csvreader)
+    
+# Get Headers
+    header = next(csvreader)
+    print(f"CSV Header: {header}")
+    
+# Collect the Votes
+    for row in csvreader:
+        total_votes = total_votes + 1
+    
+# Appending List of Candidates and Vote Totals
+        if row[2] not in candidate_list:
+            candidate_list.append(row[2])
+            votes[row[2]] = 0
+        
+        votes[row[2]] += 1
+        
+    for candidate in candidate_list:
+        percent[candidate] = round((votes[candidate] / sum(votes.values())) * 100, 3)
+    
     
 
 
