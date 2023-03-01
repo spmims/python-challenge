a3764b29c4de38cd49c1f0913432f93ffe6eda1d
spmims <stephenpmims@gmail.com>
2023-03-01 15:08:34 -0600

    Finished outside of writing to file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -35,6 +35,17 @@ with open(csvpath) as voting_file:
     for candidate in candidate_list:
         percent[candidate] = round((votes[candidate] / sum(votes.values())) * 100, 3)
     
-    
+print(total_votes, candidate_list, votes, max(votes, key = votes.get), percent, sep = "\n")    
 
+# Vote Counting for Each Candidate
+voting_counts = {}
+for name in candidate_list:
+    voting_counts[name] = str(percent[name]) + "% " + "(" + str(votes[name]) + ")"
+    
+output = "\n".join(f"{key}: {unit}" for key, unit in voting_counts.items())
+
+# Put Results into Format for Sending to File
+results = (f"Election Results\n" f"-------------------------\n" f"Total Votes: {total_votes}"
+           f"-------------------------\n" f"{output}\n" f"-------------------------\n"
+           f"Winner: {max(votes, key = votes.get)}\n" f"-------------------------\n")
 
